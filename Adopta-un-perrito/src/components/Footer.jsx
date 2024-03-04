ae0f410f0f9fa629bf3bf8a99c598c60072612f1
Sixto Obreque <sixto.obreque@gmail.com>
2024-03-03 22:06:58 -0300

    seccambia codigo jsx

--- Adopta-un-perrito/src/components/Footer.jsx
+++ Adopta-un-perrito/src/components/Footer.jsx
@@ -3,9 +3,10 @@ import React from "react";
 export default function Footer () {
     return (
         <div className="footer">
-            <h5>¡Descubre a tu compañero peludo perfecto!
-
-En nuestra página web, encontrarás una variedad de cachorros adorables esperando ser adoptados. Conecta con tu nuevo mejor amigo y llena tu vida de amor y alegría. ¡Adopta hoy y haz una diferencia!</h5>
+            <p>Explora nuestra galeria de adopcion de perros para encontrar a tu compañero perfecto. Tenemos una variedades
+          de perros con diferentes personalidades y tamaños, todos en busca de un hogar amoroso.
+          Cada imagen captura su ausencia unica. Adopta un perro y brindale una segunda oprotunidad. Encuntra a tu compañero peludo para siempre.
+        </p>
         </div>
     );
 }
